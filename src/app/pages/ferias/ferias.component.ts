369baf058f0971795bb607079e5d9b1075159914
NicolasC1906 <70461998+NicolasC1906@users.noreply.github.com>
2020-11-09 20:51:01 -0500

    feria Scroll

--- src/app/pages/ferias/ferias.component.ts
+++ src/app/pages/ferias/ferias.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Subject, Subscription } from 'rxjs';
-import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '../../services/products.service';
+import { SalesService } from '../../services/sales.service';
+import { CategoriesService } from '../../services/categories.service';
+import { from, Subject, Subscription } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
 import { TiendasService } from '../../services/tiendas.service';
+import { UsuariosService } from '../../services/usuarios.service';
+import Swal from 'sweetalert2';
 import {NgbRatingConfig} from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-ferias',
@@ -18,11 +24,23 @@ export class FeriasComponent implements OnInit {
   url:any;
   product:any = [];
   getFerias:any = [];
+  getCategorias:any = [];
+  getproducts:any = [ ];
+  getTiendas:any = [];
+  currentRate = 8;
 
-  constructor(private tiendasService: TiendasService) { }
+  constructor(
+    private CategoriesService: CategoriesService,
+    private ProductsService: ProductsService,
+    private salesService: SalesService,
+    private usuariosService: UsuariosService,
+    private tiendasService: TiendasService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     this.getUrl();
+    this.getTienda();
   }
 
   getUrl(){
@@ -44,6 +62,59 @@ export class FeriasComponent implements OnInit {
 
     }
    );
+
+
   }
+
+  getTienda(){
+    this.tiendasService
+    .getData()
+    .subscribe(resp =>{
+      let i;
+      for(i in resp){
+        this.getTiendas.push({
+
+          "nombre":resp[i].nombre,
+          "direccion": resp[i].direccion,
+          "descripcion":resp[i].descripcion,
+          "calificacion":resp[i].calificacion,
+        })
+        console.log(this.getTiendas);
+      }
+
+    }
+   );
+  }
+
+  storie(){
+    let timerInterval
+    Swal.fire({
+      html:
+    `<body>
+    <video width="320" height="240" autoplay>
+    <source src="https://lamejorferia.000webhostapp.com/Naturaleza_%20windows%207%20v%C3%ADdeo%20de%20muestra..mp4">
+    Your browser does not support the video tag.
+  </video>
+        </body>`,
+        background: 'rgba(0,0,23,0.4)',
+        backdrop: `
+                    rgba(0,0,46, 78, 255)
+                    left top
+                    no-repeat
+                  `,
+      showConfirmButton:true,
+      timer: 2000,
+      confirmButtonText:'Ver Productos',
+      timerProgressBar: true,
+      showCloseButton: true,
+      showCancelButton: false,
+      focusConfirm: false,
+      width:720,
+    })
+
+
+
+  }
+
 
 }
